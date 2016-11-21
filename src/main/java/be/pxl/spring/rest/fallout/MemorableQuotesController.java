336971e5e9e3b062d4720f4add67daae6b5df380
stefanie ramaekers <ramaekersstefanie@hotmail.com>
2016-11-21 15:58:53 +0100

    added two methods to run tests succesfully (http responsecode, requestmapping, requestbody, requestparams)

--- src/main/java/be/pxl/spring/rest/fallout/MemorableQuotesController.java
+++ src/main/java/be/pxl/spring/rest/fallout/MemorableQuotesController.java
@@ -1,14 +1,17 @@
 package be.pxl.spring.rest.fallout;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping(MemorableQuotesController.QUOTE_BASE_URL)
 @RestController
-public class MemorableQuotesController {
+public class
+MemorableQuotesController {
 
     public static final String QUOTE_BASE_URL = "/quote";
     private static final List<Quote> quotes;
@@ -22,4 +25,16 @@ public class MemorableQuotesController {
         quotes.add(Quote.of("Moira Brown", "Here, take a few radiation chems, as my little way of saying, \"I'm sorry I twisted your DNA like a kitten with a ball of yarn.\""));
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    //@ResponseStatus(value = HttpStatus.OK)
+    public List<Quote> getQuotes(@RequestParam("author") String author) {
+        return quotes.stream().filter(c -> c.getAuthor().equals(author)).collect(Collectors.toList());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Quote> insertNewQuote(@RequestBody Quote quote) {
+        quotes.add(quote);
+        return new ResponseEntity<Quote>(quote, HttpStatus.CREATED);
+    }
 }
